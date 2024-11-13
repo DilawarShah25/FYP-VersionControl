819449fe7e08889cc430e7a530b6fbcc6544604a
DilawarShah25 <csdilawar@gmail.com>
2024-11-14 00:02:31 +0500

    Well structured the project

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'dart:async'; // Import for Timer
-import 'home_screen.dart'; // Import the new file
+import 'screens/splash_screen.dart'; // Import splash screen
 
 void main() {
   runApp(const MyApp());
@@ -8,84 +7,11 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SplashScreen(),
-    );
-  }
-}
-
-class SplashScreen extends StatefulWidget {
-  const SplashScreen({super.key});
-
-  @override
-  _SplashScreenState createState() => _SplashScreenState();
-}
-
-class _SplashScreenState extends State<SplashScreen> {
-  @override
-  void initState() {
-    super.initState();
-    // Navigate to HomeScreen after 3 seconds
-    Timer(const Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => HomeScreen()),
-      );
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.transparent,
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                width: 160.0,
-                height: 160.0,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  image: const DecorationImage(
-                    image: AssetImage('assets/revivehair.png'),
-                    fit: BoxFit.cover,
-                  ),
-                  shape: BoxShape.rectangle,
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-              ),
-              const SizedBox(height: 30.0),
-              const Text(
-                'HAIR LOSS DETECTION \n& PREVENTION SYSTEM',
-                style: TextStyle(
-                fontFamily: 'Pacifico',
-                fontSize: 40.0,
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 20.0),
-              const Text(
-                'Note: This app will just provide the\nprediction, '
-                    'based on trained dataset.',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontFamily: 'sans-serif',
-                  color: Colors.white,
-                  fontSize: 15.0,
-                  letterSpacing: 1.5,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
+      home: SplashScreen(), // Use SplashScreen as the initial screen
     );
   }
 }
