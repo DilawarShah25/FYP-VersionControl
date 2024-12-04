f0668a81e237b02da073dc774b073213d4cf7d84
DilawarShah25 <csdilawar@gmail.com>
2024-12-04 17:01:37 +0500

    adjust files 1

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import ' views/onboarding/splash_view.dart'; // Import splash screen
 
 void main() {
   runApp(const MyApp());
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      home: SplashView(), // Use SplashScreen as the initial screen
     );
   }
 }
