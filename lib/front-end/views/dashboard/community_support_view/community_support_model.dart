34de4e5a2c4c478074dce5afa4a536a11d8c0e63
DilawarShah25 <csdilawar@gmail.com>
2024-12-25 16:21:09 +0500

    community support - initial

--- lib/front-end/views/dashboard/community_support_view/community_support_model.dart
+++ lib/front-end/views/dashboard/community_support_view/community_support_model.dart
@@ -1,28 +1,15 @@
-import 'dart:io';
+class ChatMessage {
+  final String userName;
+  final String message;
+  final DateTime time;
+  final bool isCurrentUser;
+  final List<String>? imageUrls;
 
-class Post {
-  final String username;
-  final String content;
-  final List<File> images;
-  final DateTime timestamp;
-
-  Post({required this.username, required this.content, required this.images, required this.timestamp});
-
-  factory Post.fromJson(Map<String, dynamic> json) {
-    return Post(
-      username: json['username'],
-      content: json['content'],
-      images: (json['images'] as List).map((imagePath) => File(imagePath)).toList(),
-      timestamp: DateTime.parse(json['timestamp']),
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'username': username,
-      'content': content,
-      'images': images.map((file) => file.path).toList(),
-      'timestamp': timestamp.toIso8601String(),
-    };
-  }
+  ChatMessage({
+    required this.userName,
+    required this.message,
+    required this.time,
+    required this.isCurrentUser,
+    this.imageUrls,
+  });
 }
--- lib/front-end/views/onboarding/splash_view.dart
+++ lib/front-end/views/onboarding/splash_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-import '../../controllers/role_controller.dart'; // Import the Registration screen
+import '../../controllers/role_controller.dart';
 
 class SplashView extends StatefulWidget {
   const SplashView({super.key});
@@ -30,7 +30,7 @@ class _SplashViewState extends State<SplashView> {
           gradient: LinearGradient(
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
-            colors: [Color(0xFF1E88E5), Color(0xFF6AB7FF)],
+            colors: [Color(0xFF8E2DE2), Color(0xFF4A00E0)], // Amazing gradient colors
           ),
         ),
         child: SafeArea(
@@ -66,6 +66,13 @@ class _SplashViewState extends State<SplashView> {
                     fontSize: 30.0,
                     color: Colors.white,
                     fontWeight: FontWeight.bold,
+                    shadows: [
+                      Shadow(
+                        blurRadius: 10.0,
+                        color: Colors.black54,
+                        offset: Offset(0, 2),
+                      ),
+                    ],
                   ),
                   textAlign: TextAlign.center,
                 ),
@@ -82,11 +89,66 @@ class _SplashViewState extends State<SplashView> {
                   ),
                 ),
                 const SizedBox(height: 30.0),
-                const CircularProgressIndicator(
-                  valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-                ),
+                const CustomLoadingIndicator(), // Custom loading indicator
               ],
             ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomLoadingIndicator extends StatefulWidget {
+  const CustomLoadingIndicator({super.key});
+
+  @override
+  _CustomLoadingIndicatorState createState() => _CustomLoadingIndicatorState();
+}
+
+class _CustomLoadingIndicatorState extends State<CustomLoadingIndicator> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 2),
+      vsync: this,
+    )..repeat(reverse: true);
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeInOut);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ScaleTransition(
+      scale: _animation,
+      child: Container(
+        width: 50.0,
+        height: 50.0,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          shape: BoxShape.circle,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              blurRadius: 8,
+              offset: const Offset(0, 4),
+            ),
+          ],
+        ),
+        child: const Center(
+          child: Icon(
+            Icons.star,
+            color: Colors.deepPurple,
+            size: 30.0,
           ),
         ),
       ),
